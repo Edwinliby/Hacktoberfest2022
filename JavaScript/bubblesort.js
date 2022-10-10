4baa2a211d2843f86de4afdd985f9cea0fd66466
debcamila <camiladeborah72@gmail.com>
2022-10-09 21:13:43 -0300

    add bubbleSort algorithm in JS

---
+++ JavaScript/bubblesort.js
@@ -0,0 +1,13 @@
+let bubbleSort = (array) => {
+  let len = array.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (array[j] > array[j + 1]) {
+        let tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+  return array;
+};

ec60c55748f3bea9cbec3c758d3de7d9b0762e2d
Vikas Jonwal <58461513+VikasJonwal@users.noreply.github.com>
2022-10-13 18:41:33 +0530

    Calender Python Project

---
+++ Python/Calender.py
@@ -0,0 +1,19 @@
+import calendar
+print("                                                  ")
+print("                                                  ")
+print("                   Welcome To Xotic Calender Sir")
+print("                                                  ")
+print("Here You Just Have To Input Year and Month Of Which You want To see Calender")
+print("                                                  ")
+print("                                                  ")
+a = int(input("Enter The Year : \n"))
+print("                                                  ")
+b = int(input("Enter The Month : \n"))
+print("                                                  ")
+print("                                                  ")
+year = a
+month = b
+print(calendar.month(year,month))
+print("                                                  ")
+print("                                                  ")
+print("                   Thank You For Using Xotic Calender")

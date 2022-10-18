3504b8cda0f9b33eae4981bc109b6008cd32ee97
Arka Prabha Roy <107761419+Arkaroy147@users.noreply.github.com>
2022-10-18 15:18:11 +0530

    Update fibonacci_sequence.c

--- C/fibonacci_sequence.c
+++ C/fibonacci_sequence.c
@@ -3,9 +3,9 @@ int main() {
     int a, b, c, n, i;
     a = 0, b = 1;
     c = a + b;
-    printf("Enter the no. of terms");
+    printf("Enter the no. of terms=");
     scanf("%d", &n);
-    printf("The fibonacci sequence is %d %d ", a, b);
+    printf("The fibonacci sequence is:  %d %d ", a, b);
     for(i = 3; i <= n; i++)
     {
         printf("%d ", c);

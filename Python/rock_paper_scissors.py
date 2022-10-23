5b68b3cf9182e854cacdee26b366a82818f56469
Akash singh <83067464+kshatriyaakash@users.noreply.github.com>
2022-10-23 21:33:08 +0530

    Create rock_paper_scissors.py

---
+++ Python/rock_paper_scissors.py
@@ -0,0 +1,57 @@
+import random
+
+import os
+
+import re
+
+os.system('cls' if os.name=='nt' else 'clear')
+
+while (1 < 2):
+
+print ("\n")
+
+print ("Rock, Paper, Scissors - Shoot!")
+
+userChoice = input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
+
+if not re.match("[SsRrPp]", userChoice):
+
+print ("Please choose a letter:")
+
+print ("[R]ock, [S]cissors or [P]aper.")
+
+continue
+
+print ("You chose: " + userChoice)
+
+choices = ['R', 'P', 'S']
+
+opponenetChoice = random.choice(choices)
+
+print ("I chose: " + opponenetChoice)
+
+if opponenetChoice == str.upper(userChoice):
+
+print ("Tie! ")
+
+elif opponenetChoice == 'R' and userChoice.upper() == 'S':
+
+print ("Scissors beats rock, I win! ")
+
+continue
+
+elif opponenetChoice == 'S' and userChoice.upper() == 'P':
+
+print ("Scissors beats paper! I win! ")
+
+continue
+
+elif opponenetChoice == 'P' and userChoice.upper() == 'R':
+
+print ("Paper beat rock, I win!")
+
+continue
+
+else:
+
+print ("You win!")

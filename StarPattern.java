6ebd8046b34869e4b70292be72ac40f2ef8e82dc
SNEHA <89248226+Sneha2319@users.noreply.github.com>
2022-10-24 12:04:59 +0530

    Create StarPattern.java

---
+++ StarPattern.java
@@ -0,0 +1,17 @@
+
+
+//To print a Star program in java//
+public class Main
+{
+	public static void main(String[] args){
+ for(int i=1; i<=4;i++){
+        for(int j=1; j<=i;j++){
+            System.out.print("*");
+        }System.out.println();                     
+    }
+    for(int i=1; i<=3;i++){
+        for(int j=3;j>=i;j--){
+            System.out.print("*");
+        }System.out.println();
+    }
+}}

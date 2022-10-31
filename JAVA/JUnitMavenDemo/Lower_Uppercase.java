dfc02e9a7ea1c6604565de94bddf4d26e140af1e
Heyyayaya <mudalinayake@gmail.com>
2022-10-31 12:12:06 +0530

    Lowercase to Uppercase convertor

---
+++ JAVA/JUnitMavenDemo/Lower_Uppercase.java
@@ -0,0 +1,15 @@
+public class Lower_Uppercase {
+
+    public static void main(String args[]){
+        String s1="hello welcome to hacktoberfest 2022 "; //entered string
+        String s1upper=s1.toUpperCase(); //convertor
+        System.out.println("Your entered : "+s1+ '\n' ); //print input string
+        System.out.println("After converted to upper case : "+'\n' +s1upper);//print converted string
+    }
+}
+
+//Sample output for the demostration
+//Your entered : hello welcome to hacktoberfest 2022 
+
+//After converted to upper case : 
+//HELLO WELCOME TO HACKTOBERFEST 2022 
